23338fdca4ec6b2fe1c4b564531b2808e06b2847
johncava <jcava@asu.edu>
2022-04-25 13:25:02 -0700

    changed .sh files

--- fair_gap_adv_mixup_experiment.sh
+++ fair_gap_adv_mixup_experiment.sh
@@ -2,11 +2,11 @@
  
 #SBATCH -N 1  # number of nodes
 #SBATCH -n 8  # number of "tasks" (default: allocates 1 core per task)
-#SBATCH -t 0-12:00:00   # time in d-hh:mm:ss
-#SBATCH -p rcgpu7      # partion
-##SBATCH -C V100
+#SBATCH -t 0-4:00:00   # time in d-hh:mm:ss
+#SBATCH -p htcgpu      # partion
+#SBATCH -C V100
 #SBATCH --gres=gpu:1  
-#SBATCH -q wildfire       # QOS
+#SBATCH -q normal       # QOS
 #SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
 #SBATCH -e slurm.%j.err # file to save job's STDERR (%j = JobId)
 #SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
--- master_fair_gap_adv_mixup_experiment.sh
+++ master_fair_gap_adv_mixup_experiment.sh
@@ -1,7 +1,7 @@
 declare -A assArray1
 assArray1[CE]="1.0"
-assArray1[ALPHA]="1.1"
-assArray1[FOCAL]="1.0 5.0"
+assArray1[ALPHA]="0.8 0.9 1.1 1.2 1.3 1.4 1.5 2.0 3.0 4.0"
+assArray1[FOCAL]="0.0 0.5 1.0 2.0 5.0"
 
 Attacks="Square"
 
@@ -15,10 +15,10 @@ for attack in $Attacks; do
     for loss in "${!assArray1[@]}"; do 
         echo $loss
         mkdir fair-gap-adv-mixup-imbalance-experiment-$loss
-        cp ./../fair_gap_adv_mixup_experiment.sh fair-gap-adv-mixup-imbalance-experiment-$loss
-        cp ./../losses.py fair-gap-adv-mixup-imbalance-experiment-$loss
-        cp ./../dataset.py fair-gap-adv-mixup-imbalance-experiment-$loss
-        cp ./../fair_gap_av_mixup.py fair-gap-adv-mixup-imbalance-experiment-$loss
+        cp ./../../fair_gap_adv_mixup_experiment.sh fair-gap-adv-mixup-imbalance-experiment-$loss
+        cp ./../../losses.py fair-gap-adv-mixup-imbalance-experiment-$loss
+        cp ./../../dataset.py fair-gap-adv-mixup-imbalance-experiment-$loss
+        cp ./../../fair_gap_av_mixup.py fair-gap-adv-mixup-imbalance-experiment-$loss
         cd fair-gap-adv-mixup-imbalance-experiment-$loss
         for param in ${assArray1[$loss]}; do
             echo $param
